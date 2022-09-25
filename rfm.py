ec46b6b1111d85f8b1ef95b2ae23c69919ab39f3
Leon Kerner <kerner@huepfer-verlag.de>
2022-09-25 04:09:28 +0200

    rfm update

--- rfm.py
+++ rfm.py
@@ -11,35 +11,60 @@ models = keras.models
 layers = keras.layers
 backend = keras.backend
 
-imp_train = 'export/training_data.xlsx'
-imp_test = 'export/test_data.xlsx'
+#########
+#TRAINING
 
-save_path = 'models/rdt'
+imp_train = 'data/training_data_edu.xlsx'
+imp_test = 'data/test_data_edu.xlsx'
 
-label = 'age'
+save_path = 'models/rfm_edu'
+
+metrics = ["mse", "mape"]
+metrics = ["mse","binary_crossentropy", "categorical_crossentropy", "accuracy"]
+metrics = ["mse", "accuracy"]
+#########
+#PREDICTION
+
+model_imp = 'models/rfm_edu_education_20220925-0358'
+imp =  'data/population_data_edu.xlsx'
+exp = 'data/population_data_edu_predict.xlsx'
+
+########
+label = 'education'
 
 task = tfdf.keras.Task.CLASSIFICATION
-task = tfdf.keras.Task.REGRESSION
+#task = tfdf.keras.Task.REGRESSION
 
 
-def build_model(hp=None):
-    model = tfdf.keras.RandomForestModel(task=task, num_trees=512, max_depth=32,)
+def build_model(task, hp=None):
+    model = tfdf.keras.RandomForestModel(task=task, num_trees=512, max_depth=64,)
     return model
 
 
 if __name__ == '__main__':
 
-    dn = deep_net()
+    mode = 'p'
 
-    if sys.argv[1] == 't':
+    dn = deep_net(task, label, dt=True)
+
+    if len(sys.argv) != 1:
+        argv = sys.argv[1]
+    else:
+        argv = None
+    
+    if argv == 't' or mode == 't':
         print('Training')
         if len(sys.argv) > 2:
-            dn.training(build_model, imp_train, imp_test, label, task, save_path, process=sys.argv[2], dt=True)
+            dn.training(build_model, imp_train, imp_test, save_path, metrics, process=sys.argv[2])
         else:
-            dn.training(build_model, imp_train, imp_test, label, task, save_path, dt=True)
+            dn.training(build_model, imp_train, imp_test, save_path, metrics)
 
-    elif sys.argv[1] == 'p':
+    elif argv == 'p' or mode == 'p':
         print('Prediction')
-        dn.prediction()
+        dn.prediction(model_imp, imp, exp)
+    elif argv == 'e' or mode == 'e':
+        print('Evaluate')
+        dn.evaluate(model_imp, imp)
     else:
         print('Miep')
+    
